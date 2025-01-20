8155b409626aea1328d6e17e13d735f7e723c1be
raksha703 <85754170+Raksha703@users.noreply.github.com>
2025-01-20 19:58:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use(bodyParser.json());
 
 app.get('/api/message/:userMessage', getMessageRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT ||  5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
